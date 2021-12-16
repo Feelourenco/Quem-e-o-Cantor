42cc16d97f5fc664a0d6dc544df0ea8caf05ebeb
Felipe <felipelourenco108@gmail.com>
2021-12-15 21:37:38 -0300

    Adicionando comandos e musicas do youtube por link

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const discord = require('discord.js');
+const { filterFormats } = require('ytdl-core');
 const client = new discord.Client();
+const ytdl = require('ytdl-core');
 const prefix = '$';
 require('dotenv').config();
+
 
 const servers = {
     'server': {
@@ -10,25 +13,62 @@ const servers = {
     }
 }
 
-//O que o bot faz assim que ficar online
+
 client.on("ready", () => {
     console.log('Advinha quem esta falando');
 });
 
-//Identifica uma mensagem ( O parametro recebe as mensagens )
+//Identifica uma mensagem
 client.on("message", async (msg) => {
 
-    //Bot só vê mensagem de um servidor, msg pessoal não e Se a mensagem não começa com o prefixo definido, ele só retorna e continua normalmente.
+    //Bot só vê mensagem de um servidor, msg pessoal não | Se a mensagem não começa com o prefixo definido, ele só retorna e continua normalmente.
     if (!msg.guild || !msg.content.startsWith(prefix)) return;
 
-    //Bot logar no servidor
-    if (msg.content == prefix + 'join') {
-        servers.server.connection = await msg.member.voice.channel.join();
+    //Verifica se está em um canal de voz.
+    if (!msg.member.voice.channel) {
+        msg.channel.send('Você precis estar conectado no canal de voz !');
+        return;
     }
 
-    if (msg.content == prefix + 'play') {
-        servidores.server.connection.play('');
+    //Tocar a música
+    if (msg.content.startsWith(prefix + 'play')) {
+        let linkMusic = msg.content.slice(6);
+        if(ytdl.validateURL(linkMusic)){
+            servers.server.dispatcher = servers.server.connection.play(ytdl(linkMusic, {filter: 'audioonly'}));
+        }
+        else {
+            msg.channel.send('Deve-se colocar um link válido do Youtube !');
+        }
     }
+
+    // Comandos
+    switch(msg.content){
+        case prefix + 'join': //Iniciar Bot
+            try {
+                servers.server.connection = await msg.member.voice.channel.join();
+            } catch (error){
+                console.log('Erro ao entrar em um canal de voz !');
+                console.log(error);
+            }
+        break;
+        case prefix + 'leave': //Tirar o bot da sala
+            msg.member.voice.channel.leave();
+            servers.server.connection = null;
+            servers.server.dispatcher = null;
+        break;
+        case prefix + 'pause': //Pausar a Música
+            servers.server.dispatcher.pause();  
+        break;
+        case prefix + 'resume': //Voltar a tocar a Música
+            servers.server.dispatcher.resume();  
+        break;
+        default:
+            return;
+    }
+
+    if(msg.content === prefix + 'resume') {
+        
+      }
 });
 
 //Autorização para fazer login no bot
